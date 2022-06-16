9d73434f35ca4f0c4542c7245fa6c3cd374bf865
Huy Long <dohuylong572000@gmail.com>
2022-06-16 18:06:34 +0700

    add banner

--- client/src/screen/slidehot/index.jsx
+++ client/src/screen/slidehot/index.jsx
@@ -1,20 +1,63 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { dataTrailer } from "../../traillerMovie/trailler";
+import { Pagination } from "swiper";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "./style.scss";
+import { FCDialog } from "../../component/FCDialog";
 export const SlideMovieHot = () => {
-    return (
-        <>
-            <Carousel>
-                <div>
-                    <p className="legend">Legend 1</p>
+  const [open, setOpen] = useState(false);
+  const [urlVideo, setUrlVideo] = useState("");
+  const handleShowTrailer = (item) => {
+    setOpen(true);
+    setUrlVideo(item);
+  };
+  const handleClose = () => {
+    setOpen(false);
+    setUrlVideo("");
+  };
+  return (
+    <>
+      <Swiper
+        pagination={true}
+        modules={[Pagination]}
+        className="mySwiper swiper-wraper"
+      >
+        <div className="">
+          {dataTrailer.map((item, index) => (
+            <>
+              <SwiperSlide key={index}>
+                <img src={item.image} alt="1" />
+                <div className="item-hover-trailer">
+                  <PlayCircleFilledWhiteIcon
+                    onClick={() => handleShowTrailer(item.iframetrailer)}
+                  />
                 </div>
-                <div>
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
-        </>
-    )
-}
+              </SwiperSlide>
+            </>
+          ))}
+        </div>
+        <FCDialog
+          size="md"
+          open={open}
+          handleClose={handleClose}
+          content={
+            <iframe
+              width="100%"
+              height="420"
+              src={urlVideo}
+              title="Phim Thế Giới Khủng Long: Lãnh Địa Trailer | KC 10.06.2022"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          }
+        />
+      </Swiper>
+    </>
+  );
+};
---
+++ client/src/traillerMovie/trailler.js
@@ -0,0 +1,12 @@
+export const dataTrailer = [
+  {
+    image:
+      "https://cdn.galaxycine.vn/media/2022/6/14/2048x682_1655188727591.jpg",
+    iframetrailer: "https://www.youtube.com/embed/zzik4JB9D1Q",
+  },
+  {
+    image:
+      "https://cdn.galaxycine.vn/media/2022/6/10/2048wx682h_1654828561391.jpg",
+    iframetrailer: "https://www.youtube.com/embed/CYmTAphAr-c",
+  },
+];
